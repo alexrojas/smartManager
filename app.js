0adf5d9c10d9eef688ba460f1bc9aebf3229cb90
alexrojas <alex.rojas@agintechllc.com>
2018-10-24 11:59:02 -0400

    google oauth done

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
+const keys = require('./config/keys')
+
 
 app.get('/', (req, res)=>{
   res.send({hi: "there"})
 })
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback"
+  }, (accessToken, refreshToken, profile, cb) => {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
+    console.log('cb', cb);
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+  }
+));
 
+
+
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+// here we are setting the listener either for local or deployed
 const PORT = process.env.PORT || "5000"
 app.listen(PORT, (req, res)=>{
   console.log("server has started")
